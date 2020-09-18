017adf279a88a8bcca4697dcf3bc36db672f3323
Jared P. Lander <jared@landeranalytics.com>
2020-09-18 09:24:04 -0400

    Switched datasets

--- code/ml2.r
+++ code/ml2.r
@@ -25,7 +25,7 @@ library(xgboost)
 library(recipes)
 library(dplyr)
 
-rec2 <- recipe(Career ~ SalaryCY + BonusCY + Years + Reports + Review + Sector + Floor, data=comp) %>% 
+rec2 <- recipe(Career ~ Years + Review + Sector + Floor, data=comp) %>% 
     themis::step_downsample(Career, under_ratio=1.15) %>% 
     # step_knnimpute(all_predictors())
     step_integer(Career, zero_based=TRUE) %>% 
@@ -50,3 +50,51 @@ head(career_y)
 tail(career_y)
 table(career_y)
 
+library(xgboost)
+
+career_xg <- xgb.DMatrix(data=career_x, label=career_y)
+career_xg
+class(career_xg)
+
+tree1 <- xgb.train(
+    data=career_xg,
+    objective='binary:logistic',
+    nrounds=1
+)
+tree1
+xgb.plot.multi.trees(tree1)
+
+library(modeldata)
+data(credit_data)
+credit_data
+
+credit_data %>% count(Status)
+
+rec3 <- recipe(Status ~ ., data=credit_data) %>% 
+    themis::step_downsample(Status, under_ratio=1.15) %>% 
+    step_integer(Status, zero_based=TRUE) %>% 
+    step_nzv(all_predictors()) %>% 
+    step_other(all_nominal(), other='misc') %>% 
+    step_dummy(all_nominal(), one_hot=TRUE)
+rec3
+prep3 <- prep(rec3)
+juice(prep3)
+
+credit_x <- juice(prep3, all_predictors(), composition='dgCMatrix')
+credit_y <- juice(prep3, all_outcomes(), composition='matrix')
+credit_x
+head(credit_y)
+
+credit_xg <- xgb.DMatrix(data=credit_x, label=credit_y)
+
+tree1 <- xgb.train(
+    data=credit_xg,
+    objective='binary:logistic',
+    nrounds=1
+)
+xgb.plot.multi.trees(tree1)
+
+
+# common problem: overfitting, unstable, high variance
+
+# two solutions: random forest, boosted tree
